bc047ab8fc0ee4d48d5ed3371fcd8f863982c89c
Andy <andyyuan.hu@gmail.com>
2026-05-02 15:08:03 +0800

    refactor: model render palettes as entries

--- src/render.rs
+++ src/render.rs
@@ -7,7 +7,9 @@ use std::thread;
 use std::time::{Duration, Instant};
 
 const EMPTY: &[u8] = b"";
+const TEXT_BLOCKS: &[u8] = b"  ";
 const CP437_BLOCKS: &[u8] = &[0xdb, 0xdb];
+const UTF8_BLOCKS: &[u8] = &[0xe2, 0x96, 0x88, 0xe2, 0x96, 0x88];
 
 pub(crate) struct RenderState {
     terminal_width: i32,
@@ -73,149 +75,172 @@ pub(crate) struct Palette {
     output: Option<&'static [u8]>,
 }
 
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+struct PaletteEntry {
+    symbol: u8,
+    value: &'static [u8],
+}
+
+impl PaletteEntry {
+    const fn new(symbol: u8, value: &'static [u8]) -> Self {
+        Self { symbol, value }
+    }
+}
+
+const XTERM_256_PALETTE: &[PaletteEntry] = &[
+    PaletteEntry::new(b',', b"\x1b[48;5;17m"),
+    PaletteEntry::new(b'.', b"\x1b[48;5;231m"),
+    PaletteEntry::new(b'\'', b"\x1b[48;5;16m"),
+    PaletteEntry::new(b'@', b"\x1b[48;5;230m"),
+    PaletteEntry::new(b'$', b"\x1b[48;5;175m"),
+    PaletteEntry::new(b'-', b"\x1b[48;5;162m"),
+    PaletteEntry::new(b'>', b"\x1b[48;5;196m"),
+    PaletteEntry::new(b'&', b"\x1b[48;5;214m"),
+    PaletteEntry::new(b'+', b"\x1b[48;5;226m"),
+    PaletteEntry::new(b'#', b"\x1b[48;5;118m"),
+    PaletteEntry::new(b'=', b"\x1b[48;5;33m"),
+    PaletteEntry::new(b';', b"\x1b[48;5;19m"),
+    PaletteEntry::new(b'*', b"\x1b[48;5;240m"),
+    PaletteEntry::new(b'%', b"\x1b[48;5;175m"),
+];
+
+const ANSI_16_PALETTE: &[PaletteEntry] = &[
+    PaletteEntry::new(b',', b"\x1b[104m"),
+    PaletteEntry::new(b'.', b"\x1b[107m"),
+    PaletteEntry::new(b'\'', b"\x1b[40m"),
+    PaletteEntry::new(b'@', b"\x1b[47m"),
+    PaletteEntry::new(b'$', b"\x1b[105m"),
+    PaletteEntry::new(b'-', b"\x1b[101m"),
+    PaletteEntry::new(b'>', b"\x1b[101m"),
+    PaletteEntry::new(b'&', b"\x1b[43m"),
+    PaletteEntry::new(b'+', b"\x1b[103m"),
+    PaletteEntry::new(b'#', b"\x1b[102m"),
+    PaletteEntry::new(b'=', b"\x1b[104m"),
+    PaletteEntry::new(b';', b"\x1b[44m"),
+    PaletteEntry::new(b'*', b"\x1b[100m"),
+    PaletteEntry::new(b'%', b"\x1b[105m"),
+];
+
+const LINUX_PALETTE: &[PaletteEntry] = &[
+    PaletteEntry::new(b',', b"\x1b[25;44m"),
+    PaletteEntry::new(b'.', b"\x1b[5;47m"),
+    PaletteEntry::new(b'\'', b"\x1b[25;40m"),
+    PaletteEntry::new(b'@', b"\x1b[5;47m"),
+    PaletteEntry::new(b'$', b"\x1b[5;45m"),
+    PaletteEntry::new(b'-', b"\x1b[5;41m"),
+    PaletteEntry::new(b'>', b"\x1b[5;41m"),
+    PaletteEntry::new(b'&', b"\x1b[25;43m"),
+    PaletteEntry::new(b'+', b"\x1b[5;43m"),
+    PaletteEntry::new(b'#', b"\x1b[5;42m"),
+    PaletteEntry::new(b'=', b"\x1b[25;44m"),
+    PaletteEntry::new(b';', b"\x1b[5;44m"),
+    PaletteEntry::new(b'*', b"\x1b[5;40m"),
+    PaletteEntry::new(b'%', b"\x1b[5;45m"),
+];
+
+const FALLBACK_PALETTE: &[PaletteEntry] = &[
+    PaletteEntry::new(b',', b"\x1b[0;34;44m"),
+    PaletteEntry::new(b'.', b"\x1b[1;37;47m"),
+    PaletteEntry::new(b'\'', b"\x1b[0;30;40m"),
+    PaletteEntry::new(b'@', b"\x1b[1;37;47m"),
+    PaletteEntry::new(b'$', b"\x1b[1;35;45m"),
+    PaletteEntry::new(b'-', b"\x1b[1;31;41m"),
+    PaletteEntry::new(b'>', b"\x1b[1;31;41m"),
+    PaletteEntry::new(b'&', b"\x1b[0;33;43m"),
+    PaletteEntry::new(b'+', b"\x1b[1;33;43m"),
+    PaletteEntry::new(b'#', b"\x1b[1;32;42m"),
+    PaletteEntry::new(b'=', b"\x1b[1;34;44m"),
+    PaletteEntry::new(b';', b"\x1b[0;34;44m"),
+    PaletteEntry::new(b'*', b"\x1b[1;30;40m"),
+    PaletteEntry::new(b'%', b"\x1b[1;35;45m"),
+];
+
+const VT220_PALETTE: &[PaletteEntry] = &[
+    PaletteEntry::new(b',', b"::"),
+    PaletteEntry::new(b'.', b"@@"),
+    PaletteEntry::new(b'\'', b"  "),
+    PaletteEntry::new(b'@', b"##"),
+    PaletteEntry::new(b'$', b"??"),
+    PaletteEntry::new(b'-', b"<>"),
+    PaletteEntry::new(b'>', b"##"),
+    PaletteEntry::new(b'&', b"=="),
+    PaletteEntry::new(b'+', b"--"),
+    PaletteEntry::new(b'#', b"++"),
+    PaletteEntry::new(b'=', b"~~"),
+    PaletteEntry::new(b';', b"$$"),
+    PaletteEntry::new(b'*', b";;"),
+    PaletteEntry::new(b'%', b"()"),
+];
+
+const VT100_ASCII_PALETTE: &[PaletteEntry] = &[
+    PaletteEntry::new(b',', b"."),
+    PaletteEntry::new(b'.', b"@"),
+    PaletteEntry::new(b'\'', b" "),
+    PaletteEntry::new(b'@', b"#"),
+    PaletteEntry::new(b'$', b"?"),
+    PaletteEntry::new(b'-', b"O"),
+    PaletteEntry::new(b'>', b"#"),
+    PaletteEntry::new(b'&', b"="),
+    PaletteEntry::new(b'+', b"-"),
+    PaletteEntry::new(b'#', b"+"),
+    PaletteEntry::new(b'=', b"~"),
+    PaletteEntry::new(b';', b"$"),
+    PaletteEntry::new(b'*', b";"),
+    PaletteEntry::new(b'%', b"o"),
+];
+
+const TRUE_COLOR_PALETTE: &[PaletteEntry] = &[
+    PaletteEntry::new(b',', b"\x1b[48;2;0;49;105m"),
+    PaletteEntry::new(b'.', b"\x1b[48;2;255;255;255m"),
+    PaletteEntry::new(b'\'', b"\x1b[48;2;0;0;0m"),
+    PaletteEntry::new(b'@', b"\x1b[48;2;255;205;152m"),
+    PaletteEntry::new(b'$', b"\x1b[48;2;255;169;255m"),
+    PaletteEntry::new(b'-', b"\x1b[48;2;255;76;152m"),
+    PaletteEntry::new(b'>', b"\x1b[48;2;255;25;0m"),
+    PaletteEntry::new(b'&', b"\x1b[48;2;255;154;0m"),
+    PaletteEntry::new(b'+', b"\x1b[48;2;255;240;0m"),
+    PaletteEntry::new(b'#', b"\x1b[48;2;40;220;0m"),
+    PaletteEntry::new(b'=', b"\x1b[48;2;0;144;255m"),
+    PaletteEntry::new(b';', b"\x1b[48;2;104;68;255m"),
+    PaletteEntry::new(b'*', b"\x1b[48;2;153;153;153m"),
+    PaletteEntry::new(b'%', b"\x1b[48;2;255;163;152m"),
+];
+
 impl Palette {
     pub(crate) fn new(terminal_type: TerminalType) -> Self {
-        let mut palette = Self {
-            colors: [EMPTY; 256],
-            output: Some(b"  "),
+        let (entries, output) = match terminal_type {
+            TerminalType::Xterm256 => (XTERM_256_PALETTE, Some(TEXT_BLOCKS)),
+            TerminalType::Ansi16 => (ANSI_16_PALETTE, Some(TEXT_BLOCKS)),
+            TerminalType::Linux => (LINUX_PALETTE, Some(TEXT_BLOCKS)),
+            TerminalType::Fallback => (FALLBACK_PALETTE, Some(UTF8_BLOCKS)),
+            TerminalType::Vtnt => (FALLBACK_PALETTE, Some(CP437_BLOCKS)),
+            TerminalType::Vt220 => (VT220_PALETTE, None),
+            TerminalType::Vt100Ascii => (VT100_ASCII_PALETTE, None),
+            TerminalType::TrueColor => (TRUE_COLOR_PALETTE, Some(TEXT_BLOCKS)),
         };
 
-        match terminal_type {
-            TerminalType::Xterm256 => {
-                palette.colors[b',' as usize] = b"\x1b[48;5;17m";
-                palette.colors[b'.' as usize] = b"\x1b[48;5;231m";
-                palette.colors[b'\'' as usize] = b"\x1b[48;5;16m";
-                palette.colors[b'@' as usize] = b"\x1b[48;5;230m";
-                palette.colors[b'$' as usize] = b"\x1b[48;5;175m";
-                palette.colors[b'-' as usize] = b"\x1b[48;5;162m";
-                palette.colors[b'>' as usize] = b"\x1b[48;5;196m";
-                palette.colors[b'&' as usize] = b"\x1b[48;5;214m";
-                palette.colors[b'+' as usize] = b"\x1b[48;5;226m";
-                palette.colors[b'#' as usize] = b"\x1b[48;5;118m";
-                palette.colors[b'=' as usize] = b"\x1b[48;5;33m";
-                palette.colors[b';' as usize] = b"\x1b[48;5;19m";
-                palette.colors[b'*' as usize] = b"\x1b[48;5;240m";
-                palette.colors[b'%' as usize] = b"\x1b[48;5;175m";
-            }
-            TerminalType::Ansi16 => {
-                palette.colors[b',' as usize] = b"\x1b[104m";
-                palette.colors[b'.' as usize] = b"\x1b[107m";
-                palette.colors[b'\'' as usize] = b"\x1b[40m";
-                palette.colors[b'@' as usize] = b"\x1b[47m";
-                palette.colors[b'$' as usize] = b"\x1b[105m";
-                palette.colors[b'-' as usize] = b"\x1b[101m";
-                palette.colors[b'>' as usize] = b"\x1b[101m";
-                palette.colors[b'&' as usize] = b"\x1b[43m";
-                palette.colors[b'+' as usize] = b"\x1b[103m";
-                palette.colors[b'#' as usize] = b"\x1b[102m";
-                palette.colors[b'=' as usize] = b"\x1b[104m";
-                palette.colors[b';' as usize] = b"\x1b[44m";
-                palette.colors[b'*' as usize] = b"\x1b[100m";
-                palette.colors[b'%' as usize] = b"\x1b[105m";
-            }
-            TerminalType::Linux => {
-                palette.colors[b',' as usize] = b"\x1b[25;44m";
-                palette.colors[b'.' as usize] = b"\x1b[5;47m";
-                palette.colors[b'\'' as usize] = b"\x1b[25;40m";
-                palette.colors[b'@' as usize] = b"\x1b[5;47m";
-                palette.colors[b'$' as usize] = b"\x1b[5;45m";
-                palette.colors[b'-' as usize] = b"\x1b[5;41m";
-                palette.colors[b'>' as usize] = b"\x1b[5;41m";
-                palette.colors[b'&' as usize] = b"\x1b[25;43m";
-                palette.colors[b'+' as usize] = b"\x1b[5;43m";
-                palette.colors[b'#' as usize] = b"\x1b[5;42m";
-                palette.colors[b'=' as usize] = b"\x1b[25;44m";
-                palette.colors[b';' as usize] = b"\x1b[5;44m";
-                palette.colors[b'*' as usize] = b"\x1b[5;40m";
-                palette.colors[b'%' as usize] = b"\x1b[5;45m";
-            }
-            TerminalType::Fallback => {
-                palette.colors[b',' as usize] = b"\x1b[0;34;44m";
-                palette.colors[b'.' as usize] = b"\x1b[1;37;47m";
-                palette.colors[b'\'' as usize] = b"\x1b[0;30;40m";
-                palette.colors[b'@' as usize] = b"\x1b[1;37;47m";
-                palette.colors[b'$' as usize] = b"\x1b[1;35;45m";
-                palette.colors[b'-' as usize] = b"\x1b[1;31;41m";
-                palette.colors[b'>' as usize] = b"\x1b[1;31;41m";
-                palette.colors[b'&' as usize] = b"\x1b[0;33;43m";
-                palette.colors[b'+' as usize] = b"\x1b[1;33;43m";
-                palette.colors[b'#' as usize] = b"\x1b[1;32;42m";
-                palette.colors[b'=' as usize] = b"\x1b[1;34;44m";
-                palette.colors[b';' as usize] = b"\x1b[0;34;44m";
-                palette.colors[b'*' as usize] = b"\x1b[1;30;40m";
-                palette.colors[b'%' as usize] = b"\x1b[1;35;45m";
-                palette.output = Some("██".as_bytes());
-            }
-            TerminalType::Vtnt => {
-                palette.colors[b',' as usize] = b"\x1b[0;34;44m";
-                palette.colors[b'.' as usize] = b"\x1b[1;37;47m";
-                palette.colors[b'\'' as usize] = b"\x1b[0;30;40m";
-                palette.colors[b'@' as usize] = b"\x1b[1;37;47m";
-                palette.colors[b'$' as usize] = b"\x1b[1;35;45m";
-                palette.colors[b'-' as usize] = b"\x1b[1;31;41m";
-                palette.colors[b'>' as usize] = b"\x1b[1;31;41m";
-                palette.colors[b'&' as usize] = b"\x1b[0;33;43m";
-                palette.colors[b'+' as usize] = b"\x1b[1;33;43m";
-                palette.colors[b'#' as usize] = b"\x1b[1;32;42m";
-                palette.colors[b'=' as usize] = b"\x1b[1;34;44m";
-                palette.colors[b';' as usize] = b"\x1b[0;34;44m";
-                palette.colors[b'*' as usize] = b"\x1b[1;30;40m";
-                palette.colors[b'%' as usize] = b"\x1b[1;35;45m";
-                palette.output = Some(CP437_BLOCKS);
-            }
-            TerminalType::Vt220 => {
-                palette.colors[b',' as usize] = b"::";
-                palette.colors[b'.' as usize] = b"@@";
-                palette.colors[b'\'' as usize] = b"  ";
-                palette.colors[b'@' as usize] = b"##";
-                palette.colors[b'$' as usize] = b"??";
-                palette.colors[b'-' as usize] = b"<>";
-                palette.colors[b'>' as usize] = b"##";
-                palette.colors[b'&' as usize] = b"==";
-                palette.colors[b'+' as usize] = b"--";
-                palette.colors[b'#' as usize] = b"++";
-                palette.colors[b'=' as usize] = b"~~";
-                palette.colors[b';' as usize] = b"$$";
-                palette.colors[b'*' as usize] = b";;";
-                palette.colors[b'%' as usize] = b"()";
-                palette.output = None;
-            }
-            TerminalType::Vt100Ascii => {
-                palette.colors[b',' as usize] = b".";
-                palette.colors[b'.' as usize] = b"@";
-                palette.colors[b'\'' as usize] = b" ";
-                palette.colors[b'@' as usize] = b"#";
-                palette.colors[b'$' as usize] = b"?";
-                palette.colors[b'-' as usize] = b"O";
-                palette.colors[b'>' as usize] = b"#";
-                palette.colors[b'&' as usize] = b"=";
-                palette.colors[b'+' as usize] = b"-";
-                palette.colors[b'#' as usize] = b"+";
-                palette.colors[b'=' as usize] = b"~";
-                palette.colors[b';' as usize] = b"$";
-                palette.colors[b'*' as usize] = b";";
-                palette.colors[b'%' as usize] = b"o";
-                palette.output = None;
-            }
-            TerminalType::TrueColor => {
-                palette.colors[b',' as usize] = b"\x1b[48;2;0;49;105m";
-                palette.colors[b'.' as usize] = b"\x1b[48;2;255;255;255m";
-                palette.colors[b'\'' as usize] = b"\x1b[48;2;0;0;0m";
-                palette.colors[b'@' as usize] = b"\x1b[48;2;255;205;152m";
-                palette.colors[b'$' as usize] = b"\x1b[48;2;255;169;255m";
-                palette.colors[b'-' as usize] = b"\x1b[48;2;255;76;152m";
-                palette.colors[b'>' as usize] = b"\x1b[48;2;255;25;0m";
-                palette.colors[b'&' as usize] = b"\x1b[48;2;255;154;0m";
-                palette.colors[b'+' as usize] = b"\x1b[48;2;255;240;0m";
-                palette.colors[b'#' as usize] = b"\x1b[48;2;40;220;0m";
-                palette.colors[b'=' as usize] = b"\x1b[48;2;0;144;255m";
-                palette.colors[b';' as usize] = b"\x1b[48;2;104;68;255m";
-                palette.colors[b'*' as usize] = b"\x1b[48;2;153;153;153m";
-                palette.colors[b'%' as usize] = b"\x1b[48;2;255;163;152m";
-            }
+        Self::from_entries(entries, output)
+    }
+
+    fn from_entries(entries: &'static [PaletteEntry], output: Option<&'static [u8]>) -> Self {
+        let mut palette = Self {
+            colors: [EMPTY; 256],
+            output,
+        };
+
+        for &entry in entries {
+            palette.set(entry);
         }
 
         palette
+    }
+
+    fn set(&mut self, entry: PaletteEntry) {
+        self.colors[entry.symbol as usize] = entry.value;
+    }
+
+    fn color(&self, symbol: u8) -> &'static [u8] {
+        self.colors[symbol as usize]
     }
 }
 
@@ -329,15 +354,16 @@ fn render_frame(
 
             match palette.output {
                 Some(output) => {
-                    if color != last && !palette.colors[color as usize].is_empty() {
+                    let escape = palette.color(color);
+                    if color != last && !escape.is_empty() {
                         last = color;
-                        out.extend_from_slice(palette.colors[color as usize]);
+                        out.extend_from_slice(escape);
                     }
                     out.extend_from_slice(output);
                 }
                 None => {
-                    // ASCII mode: palette.colors already contains the visual representation
-                    out.extend_from_slice(palette.colors[color as usize]);
+                    // ASCII mode: palette entries already contain the visual representation.
+                    out.extend_from_slice(palette.color(color));
                 }
             }
         }
@@ -430,6 +456,32 @@ mod tests {
         render_frame(&mut out, config, &state, &palette, 0, elapsed_seconds);
 
         out
+    }
+
+    #[test]
+    fn palette_uses_terminal_specific_entries() {
+        let palette = Palette::new(TerminalType::Vt100Ascii);
+
+        assert_eq!(palette.color(b','), b".");
+        assert_eq!(palette.color(b'@'), b"#");
+        assert_eq!(palette.output, None);
+    }
+
+    #[test]
+    fn block_palettes_share_colors_with_different_outputs() {
+        let fallback = Palette::new(TerminalType::Fallback);
+        let vtnt = Palette::new(TerminalType::Vtnt);
+
+        assert_eq!(fallback.color(b','), vtnt.color(b','));
+        assert_eq!(fallback.output, Some(UTF8_BLOCKS));
+        assert_eq!(vtnt.output, Some(CP437_BLOCKS));
+    }
+
+    #[test]
+    fn unknown_palette_symbols_are_empty() {
+        let palette = Palette::new(TerminalType::TrueColor);
+
+        assert_eq!(palette.color(b'?'), EMPTY);
     }
 
     #[test]
